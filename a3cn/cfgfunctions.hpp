b04bb84dd63a3e626c0e0c8a5620b1af6d6feb04
Jonathan P <jonathan.pereira@gmail.com>
2017-12-30 00:07:32 -0200

    Conceito inicial funcionado. Ainda precisa de ajustes

---
+++ a3cn/cfgfunctions.hpp
@@ -0,0 +1,23 @@
+class CfgFunctions
+{
+class A3CN
+{
+tag = "A3CN";
+
+	class transport {
+		file = "a3cn\transport";
+		class init_transport {description = "init transport option";};
+		class dest_transport {description = "transport destination";};
+		class rtb_transport {description = "transport rtb";};
+		class mappos {description = "gets pos from mapclick";};
+		class transportserver {description = "server function for airlift";};
+		class init_airlift {description = "init airlift option";};
+		class airliftserver {description = "server function for airlift";};
+		class tracker {description = "mostra onde o ponto está";};
+		class mark_point {description = "marca o ponto";};
+		class mark_global {description = "mark";};
+		class mark_local {description = "mark2";};
+	};
+};
+};
+
